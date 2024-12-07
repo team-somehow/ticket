6cda39ce24dfc3224dba24be284af4c3aa9fcd4c
Rahul D Local <dandonarahul2002@gmail.com>
2024-12-07 22:04:23 +0530

    fix: attestations

--- attestations/true-network/true.config.ts
+++ attestations/true-network/true.config.ts
@@ -4,13 +4,15 @@ import { TrueConfig } from '@truenetworkio/sdk/dist/utils/cli-config'
 
 // If you are not in a NodeJS environment, please comment the code following code:
 import dotenv from 'dotenv'
-import { fanScoreSchema } from './schemas'
+import { fanScoreSchema } from './schema'
 dotenv.config()
 
 export const getTrueNetworkInstance = async (): Promise<TrueApi> => {
   const trueApi = await TrueApi.create(config.account.secret)
+  console.log("TrueApi Instance Created");
 
   await trueApi.setIssuer(config.issuer.hash)
+  console.log("TrueApi Issuer Set");
 
   return trueApi;
 }
@@ -19,14 +21,14 @@ export const config: TrueConfig = {
   network: testnet,
   account: {
     address: 'jNpbch5QFSPLMJrwmpkiGxTtvLegA1gJHWYrN8xkSVv8B24',
-    secret: process.env.TRUE_NETWORK_SECRET_KEY ?? ''
+    secret: process.env.NEXT_PUBLIC_TRUE_NETWORK_SECRET_KEY ?? ''
   },
   issuer: {
-    name: 'true-network-attestations',
-    hash: '0xaefb4f5ffd126ef103711880b3c097a561519dedfbdd1012e3ae82fc06c6f151'
+    name: 'fanscore',
+    hash: '0x681582c4e5bdaf342dcc5cf200d1aa78d46e30c86e96688032149fa87b6ec9b0'
   },
   algorithm: {
-    id: 116,
+    id: 122,
     path: 'acm',
     schemas: [fanScoreSchema]
   },
